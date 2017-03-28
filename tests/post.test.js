55e313bfa595dae62a2bdbc3267ccc53d95ef20a
enapupe <enapupe@gmail.com>
2017-03-28 10:41:07 -0300

    Improve test with a not strict `application/json` content-type header

--- tests/post.test.js
+++ tests/post.test.js
@@ -8,16 +8,13 @@ afterAll(() => {
 
 const URL_TEST = 'http://api.localhost'
 const API_TEST = '/login/'
+const CONTENT_TYPE = { 'content-type': 'application/json; charset=whatever' }
 
 const fetch = farfetch.api(URL_TEST)
 
-const reply = function reply(url, body) {
-  return { body: JSON.parse(body), headers: this.req.headers }
-}
-
 test('post', async () => {
-  nock(URL_TEST).post(API_TEST).reply(201, reply)
+  nock(URL_TEST).post(API_TEST).reply(201, { mybody: 'sexy' }, CONTENT_TYPE)
 
   const request = await fetch.post(API_TEST, {}, { mybody: 'sexy' })
-  expect(request.body).toEqual({ mybody: 'sexy' })
+  expect(request).toEqual({ mybody: 'sexy' })
 })
